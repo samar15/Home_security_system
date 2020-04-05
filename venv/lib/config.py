1e54d34900a863fdc41bdd76ba9020cb6aebd084
samarth <samarth.singhal.cs.2016@miet.ac.in>
2020-04-05 14:36:07 +0530

    send_email

--- venv/lib/config.py
+++ venv/lib/config.py
@@ -1,4 +1,6 @@
-senders_email = 'sa5640199@gmail.com'
+email_Address = "sa5640199@gmail.com"
 password = "12345@samarth"
 recievers_email="samarth.singhal.cs.2016@miet.ac.in"
+subject="Security_Alert"
+msg="Hello"
 
---
+++ venv/lib/send_Email.py
@@ -0,0 +1,15 @@
+
+import smtplib
+import config
+def send_Email():
+    try:
+        server= smtplib.SMTP ('smtp.gmail.com',587)
+        server.ehlo()
+        server.starttls()
+        server.login(config.email_Address,config.password)
+        message = 'Subject: {}\n\n{}.format(config.subject,config.msg)'
+        server.sendmail(config.email_Address,config.recievers_email,message)
+        server.quit()
+        print("Success email is sent!")
+    except:
+        print("failed to send email")
